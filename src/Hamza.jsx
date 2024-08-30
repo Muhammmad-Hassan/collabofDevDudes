b4f69f9ec8e99fef71f54b0833b48e5687fad01d
HGhulamHamza <khawajahamza566@gmail.com>
2024-08-30 09:18:05 +0500

    Hamza Commit

---
+++ src/Hamza.jsx
@@ -0,0 +1,16 @@
+
+
+
+function Hamza() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1>Hello,I am Hamza</h1>
+        </div>
+    </>
+  )
+}
+
+export default Hamza
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Hamza from './Hamza.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Hamza />
   </StrictMode>,
 )
